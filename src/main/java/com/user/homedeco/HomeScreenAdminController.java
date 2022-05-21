1cb4bc475cc2b72bf91f8bdfa2e40a4c9685f70a
Nilgesz Timea <timea.nilgesz@student.upt.ro>
2022-05-22 01:36:09 +0300

    added view and add items option for admin

---
+++ src/main/java/com/user/homedeco/HomeScreenAdminController.java
@@ -0,0 +1,84 @@
+package com.user.homedeco;
+
+import com.user.homedeco.services.User;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomeScreenAdminController implements Initializable {
+
+    @FXML
+    private Label adminLabel;
+    @FXML
+    private Button closeButton;
+    @FXML
+    private Button homePageButton;
+    @FXML
+    private AnchorPane home;
+    @FXML
+    private BorderPane borderPane;
+    @FXML
+    private Button clientServiceButton;
+    @FXML
+    private Button ordersButton;
+
+
+
+    public void closeButtonOnAction(ActionEvent event){
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
+        Platform.exit();
+    }
+
+
+
+    public void homeScreenButtonOnAction(ActionEvent event) throws IOException {
+
+        getPage("HomeScreenAdmin");
+    }
+
+    public void shopButtonOnAction(ActionEvent event) throws IOException{
+
+        getPage("ShopScreenAdmin");
+
+    }
+    public void ordersButtonOnAction(ActionEvent event) throws IOException{
+        getPage("ViewOrdersAdmin");
+
+    }
+
+    public void clientServiceButtonOnAction(ActionEvent event) throws IOException{
+        getPage("HistoryScreenClient");
+
+    }
+
+
+
+    public void getPage(String filename) throws IOException {
+        Node node;
+        node = (Node)FXMLLoader.load(getClass().getResource(filename + ".fxml"));
+        home.getChildren().setAll(node);
+
+    }
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        adminLabel.setText("admin");
+
+    }
+}
